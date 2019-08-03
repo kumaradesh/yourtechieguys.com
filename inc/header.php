f04d6f4bb570538d5d132c4c843c850d7f5f3115
root <root@ip-10-0-0-86.us-east-2.compute.internal>
2019-08-03 06:30:56 +0000

    changes for home page

---
+++ inc/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="https://www.yourtechieguys.com/content/themes/successive/images/favicon.png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+include ("inc/header.php");
 echo "<title>Your Techie Guys</title>";
 echo "<h2>Website is under construction !!!</h2>";
+include_once("inc/footer.php")
 ?>
