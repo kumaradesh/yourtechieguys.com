a332b2d479a7e61bc3ce2eba77a93c6b81c11813
kumaradesh <www.adesh12@gmail.com>
2019-08-04 17:24:16 +0530

    logo
    logo change

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <nav class="navbar navbar-expand-lg fixed-top trans-navigation">
         <div class="container">
             <a class="navbar-brand" href="index.html">
-                <img src="assets/img/logoo.png" alt="" class="img-fluid b-logo">
+                <img src="assets/img/logo.png" alt="" class="img-fluid b-logo">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">
